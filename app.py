bc0ae13f4f578c6a65cc414527c037183f272119
Megan <megan.yeung96@gmail.com>
2025-11-11 13:16:30 -0600

    add db_create route to make basketball table

--- app.py
+++ app.py
@@ -12,3 +12,21 @@ def testing():
     conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
     conn.close()
     return "Database connection successful"
+
+@app.route("/db_create")
+def create():
+    conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+    );
+    ''')
+    
+    conn.commit()
+    conn.close()
+    return("Basketball Table Successfully Created")
