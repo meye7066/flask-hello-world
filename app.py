efc8e7c7b5f3d1015c0de20311025a0a4e3899d2
Megan <megan.yeung96@gmail.com>
2025-11-11 13:30:16 -0600

    add db_insert and db_select routes

--- app.py
+++ app.py
@@ -24,9 +24,44 @@ def create():
         City varchar(255),
         Name varchar(255),
         Number int
-    );
-    ''')
+        );
+        ''')
     
     conn.commit()
     conn.close()
     return("Basketball Table Successfully Created")
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string=" "
+    response_string +="<table>"
+    for player in records:
+        response_string +="<tr>"
+        for info in player:
+            response_string +="<td>"{}"</td>".format(info)
+        response_string +="</tr>"
+    response_string +="</table>"
+    return response_string
