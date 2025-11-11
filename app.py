8e270c73fb980d27bf712ac39ed08e75f1274baf
Megan <megan.yeung96@gmail.com>
2025-11-11 11:51:15 -0600

    create database on Render

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Megan Y. in 3308!'
+
+@app.route("/db_test")
+def db_test:
+    conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
+    conn.close()
+    return "Database connection successful"
