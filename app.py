d534141008e5ce807458752e885ab9d79253cea8
Megan <megan.yeung96@gmail.com>
2025-11-11 11:58:51 -0600

    change def to def testing()

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello, World from Megan Y. in 3308!'
 
 @app.route("/db_test")
-def db_test:
+def testing():
     conn = psycopg2.connect("postgresql://lab10_postgres_u2h2_user:VovgFrTTMOVbWjwgv3yxZXM608XkNnmK@dpg-d49mrue3jp1c73e4sa70-a/lab10_postgres_u2h2")
     conn.close()
     return "Database connection successful"
